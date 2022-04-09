3ee2fda1fa4b807d93c966913bfbaeaa380324f7
Jenna Rajani <jenna@transformdata.io>
2022-04-09 12:40:19 -0400

    remove thing

--- pages/thing.tsx
+++
@@ -1,9 +0,0 @@
-import content from 'api/utils/google/gmail/sendEmail/templates/userEmailTemplate';
-
-const Thing = () => {
-  return (
-    <div dangerouslySetInnerHTML={{__html: content}}/>
-  )
-}
-
-export default Thing;
