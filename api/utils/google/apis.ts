4a140153b9ac8e91eb2cb0f31274131d6ebd57a2
Jenna Rajani <jenna@transformdata.io>
2022-04-10 14:52:53 -0400

    Update apis.ts

--- api/utils/google/apis.ts
+++ api/utils/google/apis.ts
@@ -1,46 +1,45 @@
-import { google } from "googleapis";
-import path from 'path'
+import { google } from 'googleapis';
+import path from 'path';
 
 const scopes = [
-  "https://www.googleapis.com/auth/drive",
-  "https://mail.google.com/",
-  "https://www.googleapis.com/auth/gmail.compose",
-  "https://www.googleapis.com/auth/gmail.metadata",
-  "https://www.googleapis.com/auth/gmail.modify",
-  "https://www.googleapis.com/auth/gmail.readonly",
-  "https://www.googleapis.com/auth/gmail.send",
-]
+  'https://www.googleapis.com/auth/drive',
+  'https://mail.google.com/',
+  'https://www.googleapis.com/auth/gmail.compose',
+  'https://www.googleapis.com/auth/gmail.metadata',
+  'https://www.googleapis.com/auth/gmail.modify',
+  'https://www.googleapis.com/auth/gmail.readonly',
+  'https://www.googleapis.com/auth/gmail.send',
+];
 
 const jwtClient = new google.auth.JWT({
   email: process.env.GOOGLE_CLIENT_EMAIL as string,
-  key: process.env.GOOGLE_PRIVATE_KEY as string,
+  key: JSON.parse(process.env.GOOGLE_PRIVATE_KEY as string),
   scopes,
-  subject: "connect@eurekaholisticnutrition.com",
-  keyId: process.env.GOOGLE_PRIVATE_KEY_ID as string
+  subject: 'connect@eurekaholisticnutrition.com',
+  keyId: process.env.GOOGLE_PRIVATE_KEY_ID as string,
 });
 
 jwtClient.authorize(function (err, tokens) {
- if (err) {
-   console.log(err);
-   return;
- } else {
-   console.log("Successfully connected!");
- }
+  if (err) {
+    console.log(err);
+    return;
+  } else {
+    console.log('Successfully connected!');
+  }
 });
-
 
 const driveClient = () => {
   return google.drive({
-    version: "v3",
+    version: 'v3',
     auth: jwtClient,
-  })
+  });
 };
 
-const gmailClient = () =>  {
+const gmailClient = () => {
   return google.gmail({
-    version: "v1",
+    version: 'v1',
     auth: jwtClient,
-  })
+  });
 };
 
 export { driveClient, gmailClient };
