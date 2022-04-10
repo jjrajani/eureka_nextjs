8c2576083156b23cd429e45dffac4a18c7145c66
Jenna Rajani <jenna@transformdata.io>
2022-04-10 10:41:09 -0400

    tmp dir

--- api/routes/generate_pdf/utils/createTempFile.ts
+++ api/routes/generate_pdf/utils/createTempFile.ts
@@ -1,46 +1,37 @@
-import tmp from "tmp";
-import fs from "fs";
-import path from "path";
-import moment from "moment";
+import tmp from 'tmp';
+import fs from 'fs';
+import path from 'path';
+import moment from 'moment';
 
 const isDev = process.env.NODE_ENV === 'development';
 
-const devTempDir = path.join(
-  __dirname,
-  "..",
-  "..",
-  "..",
-  "..",
-  "/tmp"
-);
+const devTempDir = path.join(__dirname, '..', '..', '..', '..', '/tmp');
 
-const prodTempDir = path.join(
-  __dirname,
-  "..",
-  "..",
-  "..",
-  "/tmp"
-);
+const prodTempDir = path.join(__dirname, '..', '..', '..', '/tmp');
 
 export const BASE_FILE_PATH = isDev ? devTempDir : prodTempDir;
 
 const createTempFile = (fileContent: Uint8Array) => {
   return new Promise((resolve, reject) => {
     try {
-      const date = moment().format("MM_DD_YYYY");
-      const tmpdir = path.join(BASE_FILE_PATH, date);
-      if (!fs.existsSync(tmpdir)) {
-        fs.mkdirSync(tmpdir);
+      const date = moment().format('MM_DD_YYYY');
+      const tmpBaseDir = path.join(BASE_FILE_PATH);
+      if (!fs.existsSync(tmpBaseDir)) {
+        fs.mkdirSync(tmpBaseDir);
+      }
+      const tmpDatedir = path.join(BASE_FILE_PATH, date);
+      if (!fs.existsSync(tmpDatedir)) {
+        fs.mkdirSync(tmpDatedir);
       }
       tmp.file(
         {
-          postfix: ".pdf",
-          tmpdir,
+          postfix: '.pdf',
+          tmpdir: tmpDatedir,
         },
-        function _tempFileCreated(err, path, fd, /*cleanupCallback*/) {
+        function _tempFileCreated(err, path, fd /*cleanupCallback*/) {
           if (err) throw err;
-          console.log("File: ", path);
-          console.log("Filedescriptor: ", fd);
+          console.log('File: ', path);
+          console.log('Filedescriptor: ', fd);
           fs.appendFile(path, fileContent, () => {});
           resolve(path);
         }
